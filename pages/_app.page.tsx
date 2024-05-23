3a23313b1c0550fe0221cfa6b5b995f090080365
wo-o29 <woogur29@gmail.com>
2024-05-23 21:22:19 +0900

    feat: Tanstack Query 초기 설정

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,6 +1,25 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@/styles/reset.scss";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1 * 60 * 1000, // 1분
+            gcTime: 5 * 60 * 1000, // 5분
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
