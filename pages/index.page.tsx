f818896af825f3041d8d853f4f4afe71179e3909
wo-o29 <woogur29@gmail.com>
2024-05-25 15:23:25 +0900

    feat: getServerSideProps 테스트 코드

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <a href="/">초기 설정</a>;
+export const getServerSideProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const result = await res.json();
+
+  return {
+    props: { result },
+  };
+};
+
+export default function Home({ result }: any) {
+  return (
+    <>
+      <div>{`유저 ID: ${result.userId}`}</div>
+      <div>{`ID: ${result.id}`}</div>
+      <div>{`제목: ${result.title}`}</div>
+      <div>{`상태: ${result.completed}`}</div>
+    </>
+  );
 }
