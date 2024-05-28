470760924bd079488d686c698bda7d45b8f2def9
wo-o29 <woogur29@gmail.com>
2024-05-28 16:20:36 +0900

    feat: dehydrateState 전역 설정

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HydrationBoundary, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@/styles/reset.scss";
 import { useState } from "react";
@@ -16,10 +16,13 @@ export default function App({ Component, pageProps }: AppProps) {
         },
       }),
   );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Component {...pageProps} />
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </HydrationBoundary>
     </QueryClientProvider>
   );
 }
