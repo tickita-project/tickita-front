842002bb75c2b999345f5f81a6e19665d9b0741e
wo-o29 <woogur29@gmail.com>
2024-06-01 18:40:16 +0900

    refactor: 모달 타입 상수화

--- pages/dashboard/components/GroupList/index.tsx
+++ pages/dashboard/components/GroupList/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import classNames from "classnames/bind";
 
+import { MODAL_TYPE } from "@/constants/modalType";
 import { useModalStore } from "@/store/useModalStore";
 
 import styles from "./GroupList.module.scss";
@@ -19,7 +20,11 @@ export default function GroupList() {
   return (
     <div className={cn("wrap")}>
       <div className={cn("button-box")}>
-        <button type="button" className={cn("button")} onClick={() => openModal("CREATE_GROUP")}>
+        <button
+          type="button"
+          className={cn("button")}
+          onClick={() => openModal(MODAL_TYPE.CREATE_GROUP)}
+        >
           <Image src="/icons/plus-icon.svg" width={20} height={20} alt="그룹 생성 아이콘" />
           그룹 새로 만들기
         </button>
