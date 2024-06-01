fcf8fd2c0db1e56140e159699cceb3e27b79e4ab
wo-o29 <woogur29@gmail.com>
2024-06-01 18:44:32 +0900

    comment: 그룹 컬러 타입 variable 주석 추가

--- types/type.ts
+++ types/type.ts
@@ -1,14 +1,14 @@
 export type GroupColorType =
-  | "#FF7940"
-  | "#ff3333"
-  | "#D688EA"
-  | "#3360FF"
-  | "#21D53E"
-  | "#33DAFF"
-  | "#4F4F4F"
-  | "#F380B7"
-  | "#FFB723"
-  | "#32ECB4";
+  | "#FF7940" // orange500
+  | "#FF3333" // red500
+  | "#D688EA" // purple400
+  | "#3360FF" // blue500
+  | "#21D53E" // green500
+  | "#33DAFF" // sky500
+  | "#4F4F4F" // gray800
+  | "#F380B7" // pink400
+  | "#FFB723" // yellow500
+  | "#32ECB4"; // emelard500
 
 export interface UserInfoType {
   id: number;
