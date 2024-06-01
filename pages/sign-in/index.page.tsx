9f668a4516277c466517c91b5a95dea4eb44a9c2
SONAMJU <skawn1228@gmail.com>
2024-06-01 19:55:23 +0900

    refactor: 불필요한 태그 제거

--- pages/sign-in/index.page.tsx
+++ pages/sign-in/index.page.tsx
@@ -16,30 +16,28 @@ export default function SignIn() {
       <header className={cn("header")}>
         <Image src="/icons/tickita-logo.svg" alt="로고 이미지" width={60} height={22} />
       </header>
-      <main>
-        <div className={cn("container")}>
-          <div className={cn("tickita-logo")}>
-            <p>
-              복잡한 일정조율, 고민 없이
-              <br /> 바로 약속 잡고 싶을 땐
-            </p>
-            <Image
-              src="/icons/tickita-fullname-logo.svg"
-              alt="풀네임 로고 이미지"
-              width={132}
-              height={44}
-            />
-          </div>
-          <div className={cn("login-button-container")}>
-            <Link href={KAKAO_AUTH_URL} className={cn("login-button", "kakao-login-button")}>
-              <Image src="/icons/kakao-icon.svg" alt="카카오톡 아이콘" width={20} height={20} />
-              카카오로 로그인
-            </Link>
-            <Link href="/" className={cn("login-button", "google-login-button")}>
-              <Image src="/icons/google-icon.svg" alt="구글 아이콘" width={20} height={20} />
-              구글로 로그인
-            </Link>
-          </div>
+      <main className={cn("container")}>
+        <div className={cn("tickita-logo")}>
+          <p>
+            복잡한 일정조율, 고민 없이
+            <br /> 바로 약속 잡고 싶을 땐
+          </p>
+          <Image
+            src="/icons/tickita-fullname-logo.svg"
+            alt="풀네임 로고 이미지"
+            width={132}
+            height={44}
+          />
+        </div>
+        <div className={cn("login-button-container")}>
+          <Link href={KAKAO_AUTH_URL} className={cn("login-button", "kakao-login-button")}>
+            <Image src="/icons/kakao-icon.svg" alt="카카오톡 아이콘" width={20} height={20} />
+            카카오로 로그인
+          </Link>
+          <Link href="/" className={cn("login-button", "google-login-button")}>
+            <Image src="/icons/google-icon.svg" alt="구글 아이콘" width={20} height={20} />
+            구글로 로그인
+          </Link>
         </div>
       </main>
     </>
