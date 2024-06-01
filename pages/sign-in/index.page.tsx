beb2a018e3555614d4e389043e1129365913d294
SONAMJU <skawn1228@gmail.com>
2024-06-01 10:38:20 +0900

    feat: 카카오 소셜로그인 구현

--- pages/sign-in/index.page.tsx
+++ pages/sign-in/index.page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import styles from "./index.module.scss";
 
 export default function SignIn() {
+  const router = useRouter();
+
+  const handleKakaoButtonClick = () => {
+    const KAKAO_REDIRECT_URI = "http://localhost:3000/sign-in/kakao";
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+    router.push(KAKAO_AUTH_URL);
+  };
+
   return (
     <>
       <header className={styles.header}>
-        <Image src="/Icons/tickita-logo.svg" alt="logo" width={60} height={22} />
+        <Image src="/icons/tickita-logo.svg" alt="logo" width={60} height={22} />
       </header>
       <main>
         <div className={styles.container}>
@@ -16,19 +25,23 @@ export default function SignIn() {
               <br /> 바로 약속 잡고 싶을 땐
             </p>
             <Image
-              src="/Icons/tickita-fullname-logo.svg"
+              src="/icons/tickita-fullname-logo.svg"
               alt="fullname logo"
               width={132}
               height={44}
             />
           </div>
           <div className={styles["login-button"]}>
-            <button type="button" className={styles["kakao-login-button"]}>
-              <Image src="/Icons/kakao-icon.svg" alt="kakao icon" width={20} height={20} />
+            <button
+              type="button"
+              onClick={handleKakaoButtonClick}
+              className={styles["kakao-login-button"]}
+            >
+              <Image src="/icons/kakao-icon.svg" alt="kakao icon" width={20} height={20} />
               카카오로 로그인
             </button>
             <button type="button" className={styles["google-login-button"]}>
-              <Image src="/Icons/google-icon.svg" alt="kakao icon" width={20} height={20} />
+              <Image src="/icons/google-icon.svg" alt="kakao icon" width={20} height={20} />
               구글로 로그인
             </button>
           </div>
