a068d45b8374bf530fb79de1970be58ca3921b3d
SONAMJU <skawn1228@gmail.com>
2024-06-02 12:51:34 +0900

    feat: 최초 로그인 유무에 따른 페이지 이동 기능 추가

--- pages/sign-in/kakao/index.page.tsx
+++ pages/sign-in/kakao/index.page.tsx
@@ -1,18 +1,39 @@
-import { GetServerSidePropsContext } from "next";
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { useEffect } from "react";
+
+import { useRouter } from "next/router";
 
 import { instance } from "@/apis/axios";
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
+interface KakaoProps {
+  isFirst: boolean;
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<KakaoProps>> {
   const code = context.query["code"];
 
   const res = await instance.get(`/login/oauth/kakao?code=${code}`);
-  const loginData = res.data;
+  const { isFirst } = res.data;
 
   return {
-    props: {},
+    props: { isFirst: Boolean(isFirst) },
   };
 }
 
-export default function Kakao() {
+export default function Kakao({ isFirst }: KakaoProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isFirst) {
+      router.push("/profile-setup"); // TODO: 우혁님 PR merge 된 후에, 상수로 수정 예정
+    }
+
+    if (!isFirst) {
+      router.push("/");
+    }
+  }, [isFirst]);
+
   return <div>로그인 요청중입니다.</div>;
 }
