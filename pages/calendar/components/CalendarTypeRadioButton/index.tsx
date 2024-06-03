9d0ed075c2e2f9c3230f1bcecaa665c70e5a841c
jae6269 <jae6269@gmail.com>
2024-06-03 22:52:03 +0900

    feat: 캘린더 타입 라디오버튼 컴포넌트 구현, 캘린더와 연결

--- pages/calendar/components/CalendarTypeRadioButton/index.tsx
+++ pages/calendar/components/CalendarTypeRadioButton/index.tsx
@@ -1,3 +1,51 @@
-export default function CalendarTypeRadioButton() {
-  return <div>버튼</div>;
+import classNames from "classnames/bind";
+
+import styles from "./CalendarTypeRadioButton.module.scss";
+import { CalendarType } from "../../index.page";
+
+interface CalendarTypeRadioButtonProps {
+  setType: (type: CalendarType) => void;
+}
+
+const cn = classNames.bind(styles);
+
+export default function CalendarTypeRadioButton({ setType }: CalendarTypeRadioButtonProps) {
+  return (
+    <>
+      <div className={cn("container")}>
+        <input
+          id="monthly"
+          type="radio"
+          name="calendar"
+          value="월"
+          defaultChecked
+          onClick={() => {
+            setType("월");
+          }}
+        />
+        <label htmlFor="monthly">월</label>
+        <input
+          id="weekly"
+          type="radio"
+          name="calendar"
+          value="주"
+          onClick={() => {
+            setType("주");
+          }}
+        />
+        <label htmlFor="weekly">주</label>
+
+        <input
+          id="daily"
+          type="radio"
+          name="calendar"
+          value="일"
+          onClick={() => {
+            setType("일");
+          }}
+        />
+        <label htmlFor="monthly">일</label>
+      </div>
+    </>
+  );
 }
--- pages/calendar/index.page.tsx
+++ pages/calendar/index.page.tsx
@@ -2,20 +2,30 @@ import { useState } from "react";
 
 import dayjs, { Dayjs } from "dayjs";
 
+import DailyCalendar from "./components/Calendar/DailyCalendar";
 import MonthlyCalendar from "./components/Calendar/MonthlyCalendar";
-import ColorCheckBox from "@/components/ColorCheckBox";
+import WeeklyCalendar from "./components/Calendar/WeeklyCalendar";
+import CalendarTypeRadioButton from "./components/CalendarTypeRadioButton";
 import DatePicker from "@/components/DatePicker/DatePicker";
 import Header from "@/components/Header";
 
+export type CalendarType = "월" | "주" | "일";
+
 export default function CalendarPage() {
   const [focusDate, setFocusDate] = useState<Dayjs>(dayjs());
+  const [calendarType, setCalendarType] = useState<CalendarType>("월");
 
   return (
     <>
       <Header />
 
       <DatePicker selectedDay={focusDate} setSelectedDay={setFocusDate} hasNavigation={true} />
-      <MonthlyCalendar viewDate={focusDate} />
+      <main>
+        <CalendarTypeRadioButton setType={setCalendarType} />
+        {calendarType === "월" && <MonthlyCalendar viewDate={focusDate} />}
+        {calendarType === "주" && <WeeklyCalendar />}
+        {calendarType === "일" && <DailyCalendar />}
+      </main>
     </>
   );
 }
