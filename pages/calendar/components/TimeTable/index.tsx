a9878de3aed0d4bae57b2aec48f6a22a7fedb515
jae6269 <jae6269@gmail.com>
2024-06-04 23:49:40 +0900

    rename: 캘린더 컴포넌트 폴더 따로생성, 이름변경

---
+++ pages/calendar/components/TimeTable/index.tsx
@@ -0,0 +1,8 @@
+import classNames from "classnames/bind";
+
+import styles from "./TimeTable.module.scss";
+const cn = classNames.bind(styles);
+
+export default function TimeTable() {
+  return <div></div>;
+}
