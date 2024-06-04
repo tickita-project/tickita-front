44f8b68869c83cf590b84ed3220926204136120c
SONAMJU <skawn1228@gmail.com>
2024-06-04 15:12:10 +0900

    fix: 카카오 소셜로그인 시 최초 로그인 판별하는 코드 수정

--- pages/sign-in/kakao/index.page.tsx
+++ pages/sign-in/kakao/index.page.tsx
@@ -7,7 +7,7 @@ import { instance } from "@/apis/axios";
 import { PAGE_PATH } from "@/constants/pagePath";
 
 interface KakaoProps {
-  isFirst: boolean;
+  addInfoCompleted: boolean;
 }
 
 export async function getServerSideProps(
@@ -16,24 +16,24 @@ export async function getServerSideProps(
   const code = context.query["code"];
 
   const res = await instance.get(`/login/oauth/kakao?code=${code}`);
-  const { isFirst } = res.data;
+  const { addInfoCompleted } = res.data;
 
   return {
-    props: { isFirst: Boolean(isFirst) },
+    props: { addInfoCompleted: Boolean(addInfoCompleted) },
   };
 }
 
-export default function Kakao({ isFirst }: KakaoProps) {
+export default function Kakao({ addInfoCompleted }: KakaoProps) {
   const router = useRouter();
 
   useEffect(() => {
-    if (isFirst) {
+    if (addInfoCompleted === false) {
       router.push(PAGE_PATH.PROFILE_SETUP);
       return; // TODO: 우혁님 PR merge 된 후에, 상수로 수정 예정
     }
 
     router.push(PAGE_PATH.MAIN);
-  }, [isFirst]);
+  }, [addInfoCompleted]);
 
   return <div>로그인 요청중입니다.</div>;
 }
