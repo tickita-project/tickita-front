f2c424a9ec8d9aceb8c10e9285e2aefbce36b627
SONAMJU <skawn1228@gmail.com>
2024-06-05 14:12:13 +0900

    fix: api 함수 수정에 따른 prop 수정

--- pages/sign-in/kakao/index.page.tsx
+++ pages/sign-in/kakao/index.page.tsx
@@ -7,33 +7,34 @@ import { instance } from "@/apis/axios";
 import { PAGE_PATH } from "@/constants/pagePath";
 
 interface KakaoProps {
-  addInfoCompleted: boolean;
+  id: number;
+  isComplete: boolean;
 }
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext,
 ): Promise<GetServerSidePropsResult<KakaoProps>> {
-  const code = context.query["code"];
+  const { code } = context.query;
 
   const res = await instance.get(`/login/oauth/kakao?code=${code}`);
-  const { addInfoCompleted } = res.data;
+  const { id, isComplete } = res.data;
 
   return {
-    props: { addInfoCompleted: Boolean(addInfoCompleted) },
+    props: { id, isComplete },
   };
 }
 
-export default function Kakao({ addInfoCompleted }: KakaoProps) {
+export default function Kakao({ id, isComplete }: KakaoProps) {
   const router = useRouter();
 
   useEffect(() => {
-    if (addInfoCompleted === false) {
-      router.push(PAGE_PATH.PROFILE_SETUP);
-      return; // TODO: 우혁님 PR merge 된 후에, 상수로 수정 예정
+    if (isComplete === false) {
+      router.push({ pathname: PAGE_PATH.PROFILE_SETUP, query: { id } }, PAGE_PATH.PROFILE_SETUP);
+      return;
     }
 
     router.push(PAGE_PATH.MAIN);
-  }, [addInfoCompleted]);
+  }, [isComplete]);
 
   return <div>로그인 요청중입니다.</div>;
 }
