25f648b40d786cef583425e9bb9058583f02acfe
jae6269 <jae6269@gmail.com>
2024-06-05 16:26:08 +0900

    chore: jsdoc 수정

--- hooks/useOutsideClick.ts
+++ hooks/useOutsideClick.ts
@@ -6,10 +6,9 @@ type UseOutsideClickProps = {
 };
 
 /**
- * @desc 전달 받은 DOM 과 연관없는 것을 클릭한 경우 인자의 handler 호출
+ * 전달 받은 요소 밖의 것을 클릭한 경우 인자의 handler 호출
  * @param ref 기준이 되는 DOM
- * @param handler 기준이 되는 되는 DOM 외의 요소를 클릭할 경우 호출할 함수
- * -
+ * @param handler 호출할 함수
  */
 export function useOutsideClick({ ref, handler }: UseOutsideClickProps) {
   useEffect(() => {
