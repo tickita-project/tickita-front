ad476e0fbbbec9037e1e2ea63049be1741bb657c
SONAMJU <skawn1228@gmail.com>
2024-06-06 10:43:53 +0900

    refactor: classnames 적용

--- pages/profile-setup/index.page.tsx
+++ pages/profile-setup/index.page.tsx
@@ -2,10 +2,14 @@ import { GetServerSidePropsContext } from "next";
 
 import Image from "next/image";
 
+import classNames from "classnames/bind";
+
 import ProfileSetupForm from "./components/ProfileSetupForm/ProfileSetupForm";
 import { instance } from "@/apis/axios";
 
 import styles from "./index.module.scss";
+
+const cn = classNames.bind(styles);
 
 interface ProfileSetupProps {
   accountId: number;
@@ -26,13 +30,13 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 export default function ProfileSetup({ accountId, email }: ProfileSetupProps) {
   return (
     <>
-      <header className={styles.header}>
+      <header className={cn("header")}>
         <Image src="/icons/tickita-logo.svg" alt="logo" width={60} height={22} />
       </header>
       <main>
-        <div className={styles.container}>
-          <div className={styles["profile-container"]}>
-            <p className={styles.description}>
+        <div className={cn("container")}>
+          <div className={cn("profile-container")}>
+            <p className={cn("description")}>
               티키타에 합류하신 걸 환영해요
               <br />
               원활한 사용을 위해 추가 정보를 입력해주세요
