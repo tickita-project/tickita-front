a6e9e2ba2371862180d954e1596cfa0122dce595
jae6269 <jae6269@gmail.com>
2024-06-06 12:14:31 +0900

    feat: 그룹리스트, 컬러체크박스 연결

--- pages/calendar/components/CalendarGroupList/index.tsx
+++ pages/calendar/components/CalendarGroupList/index.tsx
@@ -1,9 +1,34 @@
 import classNames from "classnames/bind";
 
+import ColorCheckBox from "@/components/ColorCheckBox";
+
+import { GroupColorType } from "@/types/type";
+
 import styles from "./CalendarGroupList.module.scss";
 
 const cn = classNames.bind(styles);
 
-export default function CalendarGroupList() {
-  return <div className={cn("container")}>그룹리스트</div>;
+interface GroupType {
+  id: number;
+  groupName: string;
+  groupColor: GroupColorType;
+}
+
+interface CalendarGroupListProps {
+  groupList: GroupType[];
+}
+
+export default function CalendarGroupList({ groupList }: CalendarGroupListProps) {
+  return (
+    <div className={cn("container")}>
+      <span>현재 가입된 그룹</span>
+      <ul className={cn("group-box")}>
+        {groupList.map((group) => (
+          <li key={group.id} className={cn("group-list")}>
+            <ColorCheckBox color={group.groupColor} title={group.groupName} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- pages/calendar/components/CalendarSideBar/index.tsx
+++ pages/calendar/components/CalendarSideBar/index.tsx
@@ -3,14 +3,45 @@ import { useRef, useState } from "react";
 import Image from "next/image";
 
 import classNames from "classnames/bind";
-import { Dayjs } from "dayjs";
 
 import DatePicker, { DatePickerProps } from "@/components/DatePicker/DatePicker";
 import { useOutsideClick } from "@/hooks/useOutsideClick";
 
+import { GroupColorType } from "@/types/type";
+
 import styles from "./CalendarSideBar.module.scss";
+import CalendarGroupList from "../CalendarGroupList";
 
 const cn = classNames.bind(styles);
+
+interface mockType {
+  id: number;
+  groupName: string;
+  groupColor: GroupColorType;
+}
+const mockData: mockType[] = [
+  {
+    id: 1,
+    groupName: "그룹1",
+    groupColor: "#3360FF",
+  },
+  {
+    id: 2,
+    groupName: "그룹2",
+    groupColor: "#D688EA",
+  },
+  {
+    id: 3,
+    groupName: "그룹3",
+    groupColor: "#21D53E",
+  },
+  {
+    id: 4,
+    groupName:
+      "그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트",
+    groupColor: "#F380B7",
+  },
+];
 
 interface CalendarSideBarProps extends DatePickerProps {}
 
@@ -44,6 +75,7 @@ export default function CalendarSideBar({ selectedDay, setSelectedDay }: Calenda
       </div>
 
       <DatePicker selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
+      <CalendarGroupList groupList={mockData} />
     </aside>
   );
 }
