1cf1820ddd7d4f751c5d6eed7cc705fb0a7d062d
jae6269 <jae6269@gmail.com>
2024-06-12 19:48:47 +0900

    feat: 일간 캘린더 초기 구현 (디자인 x)

--- pages/calendar/components/Calendar/DailyCalendar/index.tsx
+++ pages/calendar/components/Calendar/DailyCalendar/index.tsx
@@ -1,10 +1,28 @@
 import classNames from "classnames/bind";
-import { Dayjs } from "dayjs";
+
+import { DAYS } from "@/constants/calendarConstants";
+import { useDateStore } from "@/store/useDateStore";
 
 import styles from "./DailyCalendar.module.scss";
 
 const cn = classNames.bind(styles);
 
+const hours = Array.from({ length: 24 }, (_, i) => i);
+
 export default function DailyCalendar() {
-  return <div>일간 캘린더</div>;
+  const { viewDate } = useDateStore();
+  return (
+    <div className={cn("container")}>
+      <p className={cn("date")}>
+        {viewDate.date()}일 {DAYS[viewDate.day()]}요일
+      </p>
+      <div className={cn("events")}>
+        {Array.from({ length: 24 }, (_, i) => (
+          <div key={i} className={cn("event-slot")}>
+            {/* 이벤트를 여기에 추가합니다 */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- pages/calendar/components/TimeTable/index.tsx
+++
@@ -1,8 +0,0 @@
-import classNames from "classnames/bind";
-
-import styles from "./TimeTable.module.scss";
-const cn = classNames.bind(styles);
-
-export default function TimeTable() {
-  return <div></div>;
-}
