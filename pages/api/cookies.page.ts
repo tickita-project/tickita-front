36c04c3a24c2aa227cad8fce4c6842d972e3f42c
wo-o29 <woogur29@gmail.com>
2024-06-13 00:13:42 +0900

    fix: origin이 있을 때 도메인 허용

--- pages/api/cookies.page.ts
+++ pages/api/cookies.page.ts
@@ -2,8 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const origin = req.headers.origin;
-  res.setHeader("Access-Control-Allow-Origin", origin!);
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
 
   const ACCESS_TOKEN = req.cookies.ACCESS_TOKEN || null;
   const REFRESH_TOKEN = req.cookies.REFRESH_TOKEN || null;
