9f4e49fed56ba9bfaac4cc71b044ddf14b7688df
SONAMJU <skawn1228@gmail.com>
2024-06-12 18:01:41 +0900

    refactor: 불필요한 코드 제거

--- pages/api/cookies.page.ts
+++ pages/api/cookies.page.ts
@@ -1,14 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  );
-
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
