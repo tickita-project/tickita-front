e7bae7a0c1d7364cd6096900d35082b796ea4f3b
SONAMJU <skawn1228@gmail.com>
2024-06-14 20:53:42 +0900

    delete: 사용하지 않는 파일 제거

--- utils/changeLocalToUTCTime.ts
+++
@@ -1,8 +0,0 @@
-const changeLocalToUTCTime = (tokenExpireAt: string) => {
-  const localData = new Date(tokenExpireAt);
-  const UTCDate = new Date(localData.getTime() - localData.getTimezoneOffset() * 60000);
-
-  return UTCDate.toUTCString();
-};
-
-export default changeLocalToUTCTime;
