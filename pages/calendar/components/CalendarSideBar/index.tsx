3ef1b5e38d2b00f15731100891d3d420959b00e6
jae6269 <jae6269@gmail.com>
2024-06-15 13:04:05 +0900

    refacotr: 그룹 리스트 mockData api 교체

--- pages/calendar/components/CalendarSideBar/index.tsx
+++ pages/calendar/components/CalendarSideBar/index.tsx
@@ -7,43 +7,10 @@ import classNames from "classnames/bind";
 import DatePicker from "@/components/DatePicker/DatePicker";
 import { useOutsideClick } from "@/hooks/useOutsideClick";
 
-import { GroupColorType } from "@/types/type";
-
 import styles from "./CalendarSideBar.module.scss";
 import CalendarGroupList from "../CalendarGroupList";
 
 const cn = classNames.bind(styles);
-
-interface mockType {
-  id: number;
-  groupName: string;
-  groupColor: GroupColorType;
-}
-const mockData: mockType[] = [
-  {
-    id: 1,
-    groupName: "그룹1",
-    groupColor: "#3360FF",
-  },
-  {
-    id: 2,
-    groupName: "그룹2",
-    groupColor: "#D688EA",
-  },
-  {
-    id: 3,
-    groupName: "그룹3",
-    groupColor: "#21D53E",
-  },
-  {
-    id: 4,
-    groupName:
-      "그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트그룹 이름이 길어지는 경우 테스트",
-    groupColor: "#F380B7",
-  },
-];
-
-interface CalendarSideBarProps {}
 
 export default function CalendarSideBar() {
   const [isCreateListVisible, setIsCreateListVisible] = useState(false);
@@ -75,7 +42,7 @@ export default function CalendarSideBar() {
       </div>
 
       <DatePicker />
-      <CalendarGroupList groupList={mockData} />
+      <CalendarGroupList />
     </aside>
   );
 }
--- pages/calendar/index.page.tsx
+++ pages/calendar/index.page.tsx
@@ -13,11 +13,11 @@ import DailyCalendar from "./components/Calendar/DailyCalendar";
 import MonthlyCalendar from "./components/Calendar/MonthlyCalendar";
 import WeeklyCalendar from "./components/Calendar/WeeklyCalendar";
 import CalendarSideBar from "./components/CalendarSideBar";
-import { getUserInfo } from "@/apis/apis";
+import { getGroupList, getUserInfo } from "@/apis/apis";
 import { setContext } from "@/apis/axios";
 import Header from "@/components/Header";
 import MetaData from "@/components/MetaData";
-import { userInfoKey } from "@/constants/queryKey";
+import { groupKey, userInfoKey } from "@/constants/queryKey";
 import { useDateStore } from "@/store/useDateStore";
 import { calculateMonthDates } from "@/utils/calculateCalendarDates";
 
@@ -34,6 +34,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   const queryClient = new QueryClient();
 
   try {
+    await queryClient.prefetchQuery({ queryKey: groupKey.lists(), queryFn: getGroupList });
     await queryClient.prefetchQuery({ queryKey: userInfoKey.info(), queryFn: getUserInfo });
 
     return {
