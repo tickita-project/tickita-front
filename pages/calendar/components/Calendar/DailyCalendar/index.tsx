9a689467b8bd21fbf46867c69ba32d6174e8b911
jae6269 <jae6269@gmail.com>
2024-06-17 08:02:06 +0900

    feat: 일간 캘린더 드래그 모달 오픈 구현

--- pages/calendar/components/Calendar/DailyCalendar/index.tsx
+++ pages/calendar/components/Calendar/DailyCalendar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 
 import classNames from "classnames/bind";
 import dayjs, { Dayjs } from "dayjs";
@@ -6,6 +6,7 @@ import { useShallow } from "zustand/react/shallow";
 
 import { DAYS, HOURS } from "@/constants/calendarConstants";
 import { MODAL_TYPE } from "@/constants/modalType";
+import useDragSelect from "@/hooks/useDragSelect";
 import { useDateStore } from "@/store/useDateStore";
 import { useModalStore } from "@/store/useModalStore";
 
@@ -14,21 +15,24 @@ import styles from "./DailyCalendar.module.scss";
 const cn = classNames.bind(styles);
 
 export default function DailyCalendar() {
-  const { focusDate } = useDateStore(useShallow((state) => ({ focusDate: state.focusDate })));
-  const { openModal } = useModalStore();
-  const { setScheduleStart, setScheduleEnd } = useDateStore(
+  const dragContainerRef = useRef<HTMLDivElement>(null);
+  const { focusDate, setScheduleStart, setScheduleEnd } = useDateStore(
     useShallow((state) => ({
+      focusDate: state.focusDate,
       setScheduleStart: state.setScheduleStart,
       setScheduleEnd: state.setScheduleEnd,
     })),
   );
+  const { openModal } = useModalStore();
 
-  const handleOpenModalClick = (hour: number) => {
+  const handleDragEnd = (draggedIndex: number[]) => {
     const today = dayjs().startOf("day");
-    setScheduleStart(today.add(hour - 1, "hour"));
-    setScheduleEnd(today.add(hour, "hour"));
+    setScheduleStart(today.add(draggedIndex[0], "hour"));
+    setScheduleEnd(today.add(draggedIndex[draggedIndex.length - 1], "hour"));
     openModal(MODAL_TYPE.SCHEDULE_CREATE);
   };
+
+  const { draggedIndex } = useDragSelect(dragContainerRef, handleDragEnd);
 
   return (
     <div className={cn("container")}>
@@ -36,14 +40,14 @@ export default function DailyCalendar() {
         {focusDate.date()} <span>{DAYS[focusDate.day()]}</span>
       </p>
       <div className={cn("all-day-schedules")}></div>
-      <div className={cn("time-scroll-container")}>
-        {HOURS.map((hour) => (
+      <div className={cn("time-scroll-container")} ref={dragContainerRef}>
+        {HOURS.map((hour, i) => (
           <div className={cn("time-block")} key={hour}>
             <p className={cn("label")}>{hour.toString().padStart(2, "0")}</p>
             <div
               key={hour}
-              className={cn("schedule-block")}
-              onClick={() => handleOpenModalClick(hour)}
+              className={cn("schedule-block", { dragged: draggedIndex.includes(i) })}
+              data-index={i}
             ></div>
           </div>
         ))}
