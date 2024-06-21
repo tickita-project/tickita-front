ffc9806618805019dd594d81889bf69689c5ba77
jae6269 <jae6269@gmail.com>
2024-06-21 18:53:08 +0900

    feat: 그룹선택까지 구현, 최대글자수 표현

--- components/Modal/Schedule/ScheduleCreate/index.tsx
+++ components/Modal/Schedule/ScheduleCreate/index.tsx
@@ -2,23 +2,27 @@ import { useState, useEffect } from "react";
 
 import Image from "next/image";
 
-import { useQueryClient } from "@tanstack/react-query";
 import classNames from "classnames/bind";
 import { useForm } from "react-hook-form";
 import { useShallow } from "zustand/react/shallow";
 
 import { getGroupInfo } from "@/apis/apis";
 import { useGetGroupList } from "@/hooks/useGetGroupList";
+import { useGetUserInfo } from "@/hooks/useGetUserInfo";
 import { useDateStore } from "@/store/useDateStore";
 import { useModalStore } from "@/store/useModalStore";
 
-import { GroupInfoType, SchedulePostDataType } from "@/types/type";
+import { GroupInfoType, GroupType, SchedulePostDataType } from "@/types/type";
 
 import styles from "./ScheduleCreateModal.module.scss";
 
 const cn = classNames.bind(styles);
 
 export default function ScheduleCreateModal() {
+  const [isGroupListVisible, setIsGroupListVisible] = useState(false);
+  const [groupInfo, setGroupInfo] = useState<GroupInfoType | null>(null);
+  const [selectedGroup, setSelectedGroup] = useState<GroupType | null>(null);
+
   const { closeModal } = useModalStore();
   const { scheduleStart, scheduleEnd, setScheduleStart, setScheduleEnd } = useDateStore(
     useShallow((state) => ({
@@ -28,11 +32,14 @@ export default function ScheduleCreateModal() {
       setScheduleEnd: state.setScheduleEnd,
     })),
   );
+
   const {
     watch,
+    reset,
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    setValue,
+    formState: { isValid },
   } = useForm<SchedulePostDataType>({
     defaultValues: {
       startDateTime: scheduleStart?.format("YYYY-MM-DDTHH:mm"),
@@ -40,31 +47,44 @@ export default function ScheduleCreateModal() {
     },
   });
   const { data: groupList } = useGetGroupList();
-  const crewIdWatch = watch("crewId");
-  const [groupInfo, setGroupInfo] = useState<GroupInfoType | null>(null);
+  const { data: userInfo } = useGetUserInfo();
 
-  useEffect(() => {
-    //그룹이 선택되면, 아래 코드가 실행되도록 useEffect 설정
-    if (!crewIdWatch) {
-      return;
-    }
-    const fetchGroupInfo = async () => {
-      const data = await getGroupInfo(crewIdWatch);
-      setGroupInfo(data);
-    };
-    fetchGroupInfo();
-  }, [crewIdWatch]);
+  const [crewIdWatch, locationWatch = "", descriptionWatch = ""] = watch([
+    "crewId",
+    "location",
+    "description",
+  ]);
+
+  const handleGroupSelect = (group: GroupType) => {
+    setSelectedGroup(group);
+    setValue("crewId", group.crewId);
+    setIsGroupListVisible(false);
+  };
 
   const handleCloseModal = () => {
     closeModal();
     setScheduleStart(null);
     setScheduleEnd(null);
+    reset();
   };
 
   const handleCreateSchedule = (data: SchedulePostDataType) => {
     // 일정 생성 구현
     console.log(data);
   };
+
+  useEffect(() => {
+    //그룹이 선택되면, 아래 코드가 실행되도록 useEffect 설정
+    if (!crewIdWatch) {
+      return;
+    }
+
+    const fetchGroupInfo = async () => {
+      const data = await getGroupInfo(crewIdWatch);
+      setGroupInfo(data);
+    };
+    fetchGroupInfo();
+  }, [crewIdWatch]);
 
   return (
     <div className={cn("container")}>
@@ -84,11 +104,11 @@ export default function ScheduleCreateModal() {
             <p className={cn("label")}>시간</p>
             <div className={cn("time")}>
               <div className={cn("start")}>
-                <p className={cn("date")}>{scheduleStart?.format("YYYY.MM.DD")}</p>
+                <p className={cn("date")}>{scheduleStart?.format("YYYY.MM.DD HH:mm")}</p>
               </div>
               <p> - </p>
               <div className={cn("end")}>
-                <p className={cn("date")}>{scheduleEnd?.format("YYYY.MM.DD")}</p>
+                <p className={cn("date")}>{scheduleEnd?.format("YYYY.MM.DD HH:mm")}</p>
               </div>
             </div>
           </div>
@@ -99,10 +119,13 @@ export default function ScheduleCreateModal() {
               <input
                 className={cn("place-input")}
                 type="text"
-                maxLength={20}
+                maxLength={19}
                 placeholder="(선택) 위워크 10층 미팅룸"
                 {...register("location")}
               />
+              <p className={cn("location-length")}>
+                <b>{locationWatch.length}</b>/ 20
+              </p>
             </div>
           </div>
         </div>
@@ -110,42 +133,70 @@ export default function ScheduleCreateModal() {
           <p className={cn("label")}>추가 내용</p>
           <textarea
             className={cn("text")}
-            maxLength={50}
+            maxLength={79}
             placeholder="(선택) 설명을 입력해주세요"
             {...register("description")}
           />
+          <p className={cn("text-length")}>
+            <b>{descriptionWatch.length}</b>/ 80
+          </p>
         </div>
         <div className={cn("group")}>
           <p className={cn("label")}>그룹 선택</p>
-          <select className={cn("group-select")} {...register("crewId", { required: true })}>
-            <option className={cn("default-option")} value="" disabled>
-              그룹을 선택하세요
-            </option>
-            {groupList?.map((group) => (
-              <option key={group.crewId} value={group.crewId} className={cn("option")}>
+          <div
+            className={cn("group-select")}
+            onClick={() => setIsGroupListVisible(!isGroupListVisible)}
+          >
+            {selectedGroup ? (
+              <div className={cn("selected-group")}>
                 <div
                   className={cn("group-color")}
-                  style={{ backgroundColor: `${group.labelColor}` }}
+                  style={{ backgroundColor: selectedGroup.labelColor }}
                 />
-                <p className={cn("group-name")}>{group.crewName}</p>
-              </option>
-            ))}
-          </select>
+                <p className={cn("group-name")}>{selectedGroup.crewName}</p>
+              </div>
+            ) : (
+              <p className={cn("placeholder")}>그룹을 선택하세요</p>
+            )}
+          </div>
+          {isGroupListVisible && (
+            <div className={cn("group-container")}>
+              {groupList ? (
+                groupList.map((group) => (
+                  <div
+                    key={group.crewId}
+                    className={cn("group-list")}
+                    onClick={() => handleGroupSelect(group)}
+                  >
+                    <div
+                      className={cn("group-color")}
+                      style={{ backgroundColor: `${group.labelColor}` }}
+                    />
+                    <p className={cn("group-name")}>{group.crewName}</p>
+                  </div>
+                ))
+              ) : (
+                <div>그룹을 먼저 만들어주세요</div>
+              )}
+            </div>
+          )}
         </div>
         <div className={cn("member-container")}>
           <p className={cn("label")}>참가자 선택</p>
           <div className={cn("members")}>
-            {crewIdWatch &&
-              groupInfo?.crewMembers.map((member: any) => (
-                <label className={cn("member-checkbox")} key={member.accountId}>
-                  <input type="checkbox" value={member.accountId} {...register("participants")} />
-                  {member.nickName}
-                </label>
-              ))}
+            {groupInfo?.crewMembers.map(
+              (member) =>
+                member.accountId !== userInfo?.accountId && (
+                  <label className={cn("member-checkbox")} key={member.accountId}>
+                    <input type="checkbox" value={member.accountId} {...register("participants")} />
+                    {member.nickName}
+                  </label>
+                ),
+            )}
           </div>
         </div>
-        <button type="submit" disabled={!isValid} className={cn("")}>
-          일정 생성
+        <button type="submit" disabled={!isValid} className={cn("submit")}>
+          일정 등록
         </button>
       </form>
     </div>
