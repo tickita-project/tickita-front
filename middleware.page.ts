b14393ca3a0820d91769d1006096ebf3ff11af0a
SONAMJU <skawn1228@gmail.com>
2024-06-24 05:49:04 +0900

    refactor: middleware 및 로그인 로직 보완

--- middleware.page.ts
+++ middleware.page.ts
@@ -6,16 +6,20 @@ export const middleware = (request: NextRequest) => {
   const REFRESH_TOKEN = request.cookies.get("REFRESH_TOKEN");
 
   if (!REFRESH_TOKEN) {
-    return NextResponse.redirect(new URL(PAGE_PATH.SIGN_IN, request.url));
+    if (!request.nextUrl.pathname.startsWith(PAGE_PATH.SIGN_IN) && !request.nextUrl.pathname) {
+      return NextResponse.redirect(new URL(PAGE_PATH.SIGN_IN, request.url));
+    }
   }
 
-  if (REFRESH_TOKEN && request.nextUrl.pathname.startsWith(PAGE_PATH.SIGN_IN)) {
-    return NextResponse.redirect(new URL(PAGE_PATH.DASHBOARD, request.url));
+  if (REFRESH_TOKEN) {
+    if (request.nextUrl.pathname.startsWith(PAGE_PATH.SIGN_IN)) {
+      return NextResponse.redirect(new URL(PAGE_PATH.DASHBOARD, request.url));
+    }
   }
 
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ["/((?!sign-in/.*$).*)"], // "sign-in/kakao"처럼 sign-in 뒤에 path 붙는 경로 제외
+  matcher: ["/:path*"],
 };
