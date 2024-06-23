761bb995832aaa83acd1efc6fb9ac322f2fc8214
wo-o29 <woogur29@gmail.com>
2024-06-24 03:08:57 +0900

    feat: 스로톨링 추가

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,7 @@ echo "최신 변경 사항 가져오기..."
 git pull origin develop
 
 echo "의존성 설치..."
-export NODE_ENV=production # 배포 환경에서만 필요한 의존성 설치
-npm ci --only=production # 빌드 결과와 종속성을 캐시하여 반복 빌드 시간 단축, .next/cache에 저장 후 재사용
+sudo npm install
 
 echo "빌드 실행 중..."
 npm run build
